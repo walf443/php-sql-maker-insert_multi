d586ab83f536b3163e8996dc4734792ec9ef5736
Keiji, Yoshimi <walf443@gmail.com>
2013-01-29 10:17:13 +0900

    remove bind() interface.
    bind() interface may cause some mistake. ( for example, wrong number of binds)
    And Can't find this mistake except by hands.
    So, It's better to set with bindRow at once for each row, I think.

--- src/SQL/Maker/InsertMulti.php
+++ src/SQL/Maker/InsertMulti.php
@@ -4,11 +4,13 @@
  * @example
  *
  *      // simple case.
- *      $builer = new SQL_Maker_InsertMulti("your_table", array('fields' => array('id', 'name', 'created_at'));
+ *      $builder = new SQL_Maker_InsertMulti("your_table", array('fields' => array('id', 'name', 'created_at'));
  *      foreach ( $data as $row ) {
- *          $builer->bind($row['id']);
- *          $builer->bind($row['name']);
- *          $builer->bind($row['created_at']);
+ *          $builder->bindRow(array(
+ *              'id'            => $row['id'],
+ *              'name'          => $row['id'],
+ *              'created_at'    => $row['created_at'],
+ *          );
  *      }
  *
  *      $stmt = $pdo->prepare($builer->toQuery());
@@ -29,10 +31,10 @@
  *      $pdo->execute();
  */
 class SQL_Maker_InsertMulti {
-    private $binds;
     private $quoteChar;
     private $tableName;
     private $fields;
+    private $binds;
 
     function __construct($tableName, $options) {
         $default_options = array(
@@ -51,16 +53,36 @@ class SQL_Maker_InsertMulti {
         $this->quoteChar = $options['quoteChar'];
     }
 
-    public function bind($value) {
-        array_push($this->binds, $value);
+    public function bindRow(array $row) {
+        foreach ( $this->fields as $field ) {
+            if ( !array_key_exists($field, $row) ) {
+                throw new InvalidArgumentException("\$row should have '$field' field");
+            } else {
+                array_push($this->binds, $row[$field]);
+            }
+        }
+
+        return true;
     }
 
-    public function binds(){
+    public function binds()
+    {
         return $this->binds;
     }
 
     public function toQuery()
     {
+
+        $bindCount = count($this->binds);
+        if ( $bindCount === 0 ) {
+            throw new \LogicException("There are no binds");
+        }
+        $fieldCount = count($this->fields);
+        $rowCount = $bindCount / $fieldCount;
+        if ( $bindCount % $fieldCount !== 0 ) {
+            throw new \LogicException("Invalid count of binds: got " . $bindCount . ", but expected " . $fieldCount . " * " . $rowCount . " = " . $fieldCount * $rowCount);
+        }
+
         $result = "INSERT INTO " . $this->quote($this->tableName) . " ";
         $quoted_fields = array();
 
--- tests/SQL/Maker/InsertMultiTest.php
+++ tests/SQL/Maker/InsertMultiTest.php
@@ -6,14 +6,17 @@ require_once(__DIR__ . "/../../../src/SQL/Maker/InsertMulti.php");
 
 final class SQL_Maker_InsertMulti_Test extends \PHPUnit_Framework_TestCase
 {
-    function test_functional(){
+    function test_functional__in_case_simple(){
         $builder = new SQL_Maker_InsertMulti('example_table', array('fields' => array('foo', 'bar', 'baz')));
         for ( $i = 1; $i <= 3; $i++ ) {
-            $builder->bind($i*1);
-            $builder->bind($i*2);
-            $builder->bind($i*3);
+            $builder->bindRow(array(
+                'foo' => $i * 1,
+                'bar' => $i * 2,
+                'baz' => $i * 3,
+            ));
         }
         $this->assertSame($builder->toQuery(), "INSERT INTO `example_table` (`foo`, `bar`, `baz`) VALUES (?, ?, ?), (?, ?, ?), (?, ?, ?)", "query OK");
         $this->assertSame(3*3, count($builder->binds()), "binds count OK");
     }
+
 }
